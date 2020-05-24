90eac8105fdba01492c9dbcddbda1bd4addf9600
EldhosAji <eldhosaji1998@gmail.com>
2020-05-24 12:28:42 +0530

    CodeBlock update

--- src/res/codeBlock.js
+++ src/res/codeBlock.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
-import './code.css';
 import Login from './login';
 import Registration from './registration';
 import CodeTag from './codeTag';
 import { wait } from '@testing-library/react';
+
+import { Redirect } from 'react-router-dom'
 class CodeBlock extends Component {
     constructor(props){
     super(props);
         this.state={
             codeVal:'',
-            debug:''
+            debug:'',
+            initLog:false,
     };
     };
 s
@@ -39,8 +41,15 @@ s
         this.setState({login:false})
         this.setState({reg:false})
     }
+    
     _compile=()=>{
-        const data=""
+        this.setState({
+                    debug:"[*]Compiling..."
+                })
+        var x= this.state.codeVal;
+        const y  =x.replace(/\n/g, ";")
+        console.log(y)
+        console.log("csd"+sessionStorage.getItem('ideVal'))
         console.log(this.state.codeVal);
             fetch('http://localhost:8080/api/compile',{
             method:'POST',    
@@ -48,7 +57,8 @@ s
                 'Content-Type':'application/json'
             },
             body:JSON.stringify({
-                code:this.state.codeVal,
+                code:y,
+                id:sessionStorage.getItem('ideVal'),
             })
             }).then((response)=>response.json())
                 .then((response)=>
@@ -64,22 +74,41 @@ s
     _handleChange(event) {
         this.setState({codeVal: event.target.value});
     }
+
+    fetchFileCont=()=>{
+    console.log("done")
+    fetch('http://localhost:8080/api/user/code',{
+            method:'POST',    
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({
+                id:sessionStorage.getItem('ideVal')
+            })
+        }).then((response)=>response.json())
+        .then((response)=>{
+            console.log(response.code)
+            this.setState({codeVal:response.code})
+        })
+        .catch(error=>console.log(error))
+}
 render(){
-    
+    if(!sessionStorage.getItem('authKey')) return <Redirect to='/'/>
+    if(!this.state.initLog){this.fetchFileCont();this.setState({initLog:true})}
   return (
     <div >
         
       <div className='header'>
           <span id="logo" >SkyShare</span>
           <div id="tab-btns">
-          <span id="tabs" onClick={this._showHome}>Profile</span>
-          <span id="tabs" onClick={this._showLogin}>Logout</span>
+          <span id="tabs" onClick={<Redirect to='/profile'/>}><a style={{textDecoration:'none',color:'#fff'}} href='/profile'>Profile</a></span>
+          <span id="tabs" onClick={()=>{this.setState({codeVal:'',debug:''})}}>Clear IDE</span>
           </div>
       </div>
-      <button onClick={this._compile} style={{margin:10,borderWidth:0,backgroundColor:'#002DF5',color:'#fff',width:'80px',height:'40px'}}>Run</button>
+      <button onClick={this._compile} style={{margin:10,borderWidth:0,position:'absolute',borderRadius:9,float:'left',marginLeft:'50%',backgroundColor:'#0001ff',color:'#fff',width:'80px',height:'40px',fontSize:'20px'}}>Run</button>
       <div className="code-block-body" >
-        <textarea rows="30" cols="100" value={this.state.codeVal} onChange={(event) => this.setState({codeVal:event.target.value})}></textarea>
-        <textarea disabled rows="5" cols="100" value={this.state.debug}></textarea>
+        <textarea onLoad={()=>{this.fetchFileCont()}} placeholder="Enter your code" rows="30" cols="100" value={this.state.codeVal} onChange={(event) => this.setState({codeVal:event.target.value})}></textarea>
+        <textarea placeholder=">Run your code" disabled rows="5" cols="100" value={this.state.debug}></textarea>
       </div>
     </div>
   );
